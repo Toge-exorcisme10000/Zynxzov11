99cbf1446890cc87f97703ed2009f645609b8fee
Toge-exorcisme10000 <zenitsuinumaki57@gmail.com>
2024-10-15 22:21:26 +0200

    Update setting.js

--- setting.js
+++ setting.js
@@ -46,14 +46,14 @@ global.chatgpt = true // false = off, true = on
 //—————「 BOT 」—————//
 global.saluran = 'https://whatsapp.com/channel/0029ValRq0ULikgDaQ5Y0c0L' // Opsional 
 global.idsal = "@newsletter" // Jangan Di ubah
-global.botname = 'Zʏɴxᴢᴏ' // Ini nama bot
-global.bottz = '6285789034010' // Ganti dengan nomor bot untuk mendapatkan code pairing 
-global.packname = '6285789034010' // wm sticker
+global.botname = 'INUMAKI Zʏɴxᴢᴏ' // Ini nama bot
+global.bottz = '25767334893' // Ganti dengan nomor bot untuk mendapatkan code pairing 
+global.packname = '25767334893' // wm sticker
 global.author = `Date: ${moment.tz('Asia/Jakarta').format('DD/MM/YY')}\nCreator: Zʏɴxᴢᴏ \nBot: 0857-8903-4010` // wm sticker
 global.prefa = ['','!','.',',','🐤','🗿']
 global.versions = `${zynz}`
-global.ownername = 'Zʏɴxᴢᴏ' // Ubah jadi nama lu
-global.owner = ['6285789034010'] // Jangan ubah no lu
+global.ownername = 'TOGE INUMAKI' // Ubah jadi nama lu
+global.owner = ['25767334893'] // Jangan ubah no lu
 global.ownermail = ['ZynxzXx99@gmail.com'] // Opsional
 
 //—————「 REPLY 」—————//
